7def6f1d05f6028f3a5a00777bd1bb3ae8ea7e54
Raghav Agrawal <raghavagr2024@gmail.com>
2022-12-19 18:29:08 -0600

    work on EditPage

--- lib/editAdminPage.dart
+++ lib/editAdminPage.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 Map foodItems = {};
+var _search = TextEditingController();
 Future<void> getFoodItems() async {
     FirebaseFirestore.instance
       .collection('everyday')
@@ -75,6 +76,7 @@ Future<void> getFoodItems() async {
     });
 
 }
+
 class EditPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -85,6 +87,86 @@ class EditPage extends StatefulWidget {
 class _EditPage extends State<EditPage> {
   @override
   Widget build(BuildContext context) {
-    return Text(foodItems.toString());
+    return Column(
+      children: [
+        const SizedBox(height: 40,),
+        Align(
+          alignment: Alignment.center,
+          child: Container(
+              height: 40,
+              width: 380,
+              color: Colors.white,
+              alignment: Alignment.center,
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    border:Border.all(color: Colors.grey, width: 2)),
+                child: TextFormField(
+                  textAlign: TextAlign.center,
+                  controller: _search,
+                  onFieldSubmitted: (_search){
+                    print(_search.toString());
+                  },
+                  decoration: const InputDecoration(
+                    hintText: 'Search for an item',
+                    labelStyle: TextStyle(color: Colors.greenAccent),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.transparent),
+                    ),
+                  ),
+                ),
+              )
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+              itemBuilder: _getMajorListViews,
+              itemCount: foodItems.length,
+          )
+        )
+
+      ],
+    );
+  }
+
+  Widget _getMajorListViews(BuildContext context, index){
+    return MajorListView(context,index);
   }
 }
+
+class MajorListView extends StatefulWidget{
+  BuildContext context;
+  int index;
+
+  MajorListView(this.context,this.index);
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MajorListView(context,index);
+  }
+
+}
+
+class _MajorListView extends State<MajorListView>{
+  BuildContext context;
+  int parent;
+
+  _MajorListView(this.context,this.parent);
+  @override
+  Widget build(BuildContext context) {
+    Map currentView = foodItems[foodItems.keys.elementAt(parent)];
+    // return
+    //   Expanded(child: ListView.builder(
+    //     itemCount: currentView.length,
+    //     itemBuilder: (BuildContext context, int index){
+    //       return ListTile(
+    //           title: Text("hello")
+    //       );
+    //     },
+    //
+    //   ));
+    return Text(currentView.length.toString());
+  }
+
+}
+
