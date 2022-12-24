a702071a7e87505a4e9ede276f6ae3f95b7dbf5c
Raghav <raghavagr2024@gmail.com>
2022-12-23 23:24:00 -0600

    started working on edit and delete functions

--- lib/editAdminPage.dart
+++ lib/editAdminPage.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 Map foodItems = {};
 var _search = TextEditingController();
@@ -120,10 +121,11 @@ class _EditPage extends State<EditPage> {
               )),
         ),
         Expanded(
-            child: ListView.builder(
-          itemBuilder: _getMajorListViews,
-          itemCount: foodItems.length,
-        ))
+          child: ListView.builder(
+            itemBuilder: _getEveryDayItems,
+            itemCount: foodItems['everyday'].length,
+          ),
+        )
       ],
     );
   }
@@ -131,6 +133,107 @@ class _EditPage extends State<EditPage> {
   Widget _getMajorListViews(BuildContext context, index) {
     return MajorListView(context, index);
   }
+
+  Widget _getEveryDayItems(BuildContext context, int index) {
+    return EveryDayItems(context, index);
+  }
+}
+
+class EveryDayItems extends StatefulWidget {
+  BuildContext context;
+  int category;
+
+  EveryDayItems(this.context, this.category);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _EveryDayItems(this.context, this.category);
+  }
+}
+
+class _EveryDayItems extends State<EveryDayItems> {
+  BuildContext context;
+  int category;
+
+  _EveryDayItems(this.context, this.category);
+
+  Map foods = foodItems['everyday'];
+
+  @override
+  Widget build(context) {
+    var format = NumberFormat.currency(symbol: "\$", decimalDigits: 2);
+    Map currentCategory = foods.values.elementAt(category);
+    print(currentCategory.toString());
+    print("current category");
+    return Container(
+      height: currentCategory.length * 100,
+      child: ListView.builder(
+        physics: const NeverScrollableScrollPhysics(),
+        scrollDirection: Axis.vertical,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+              title: Text(currentCategory.keys.elementAt(index)),
+              subtitle:
+                  Text(format.format(currentCategory.values.elementAt(index))),
+              trailing: Container(
+                width: 100,
+                child: Row(
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          print("edit");
+                        },
+                        icon: Icon(Icons.edit)),
+                    IconButton(
+                        onPressed: () {
+                          _deleteItemDialog(currentCategory.keys.elementAt(index));
+                        },
+                        icon: Icon(Icons.delete))
+                  ],
+                ),
+              ));
+        },
+        itemCount: currentCategory.length,
+      ),
+    );
+  }
+  Future<void> _deleteItemDialog(String s) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Delete item ${s}?"),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text('Deleting this item will remove it permanently'),
+
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Deny'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Approve',style: TextStyle(color: Colors.red),),
+              onPressed: () {
+                FirebaseFirestore.instance.collection('everyday').doc('IppA94yUj2wrIzawr5Al').update({'${foods.keys.elementAt(category)}.$s': FieldValue.delete()}).whenComplete((){
+                  print('Field Deleted');
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
 }
 
 class MajorListView extends StatefulWidget {
@@ -165,17 +268,15 @@ class _MajorListView extends State<MajorListView> {
           itemBuilder: (context, index) {
             Map currentItems = currentView[currentView.keys.elementAt(index)];
             return ListView.builder(
-                itemCount: currentItems.length,
-                itemBuilder: (context,index){
-                  return ListTile(
-                    title: Text(currentItems.keys.elementAt(index)),
-                  );
-                },
-                
+              itemCount: currentItems.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(currentItems.keys.elementAt(index)),
+                );
+              },
             );
           },
-        )
-    );
+        ));
     return Text(currentView.length.toString());
   }
 }
