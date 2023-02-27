e8dc8dce6af4babddae12c18e192166b54e575e8
Raghav <raghavagr2024@gmail.com>
2023-02-26 20:42:26 -0600

    work on limits for items

--- lib/order.dart
+++ lib/order.dart
@@ -9,16 +9,59 @@ class Order {
   int id;
   Map soups;
 
-  Order(this.foods,this.soups, this.orderTime, this.orderLocation, this.paymentMethod,
-      this.id);
+  Order(this.foods, this.soups, this.orderTime, this.orderLocation,
+      this.paymentMethod, this.id);
 
   Future<void> addOrder() async {
     CollectionReference users = FirebaseFirestore.instance.collection('users');
     print("in add order");
-    return users.doc("SxHI0lmZHaO8r2BnwtuH/").update({
-      id.toString(): {"foods":foods,"soups":soups,"location":orderLocation,"time":orderTime,"payment method":paymentMethod}
-        }
-    ).then((value) => print("User Added"))
+    addCount();
+    return users
+        .doc("SxHI0lmZHaO8r2BnwtuH/")
+        .update({
+          id.toString(): {
+            "foods": foods,
+            "soups": soups,
+            "location": orderLocation,
+            "time": orderTime,
+            "payment method": paymentMethod
+          }
+        })
+        .then((value) => print("User Added"))
         .catchError((error) => print("Failed to add user: $error"));
+
+  }
+
+  Future<void> addCount() async {
+    Map limits = {};
+    await FirebaseFirestore.instance
+        .collection('limits')
+        .doc("kZ5NRpx5WBxLOLwKPWwQ")
+        .get()
+        .then((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+        limits = documentSnapshot.data() as Map<dynamic, dynamic>;
+      } else {
+        print('Document does not exist on the database');
+      }
+      print("done with getData");
+    });
+    CollectionReference reference =
+        FirebaseFirestore.instance.collection('limits');
+    for (int i = 0; i < foods.length; i++) {
+      if (limits.containsKey(foods.keys.elementAt(i))) {
+        reference
+            .doc('kZ5NRpx5WBxLOLwKPWwQ')
+            .update({'${foods.keys.elementAt(i)}': limits[foods.keys.elementAt(i)]+foods[foods.keys.elementAt(i)][0]})
+            .then((value) => print("User Added"))
+            .catchError((error) => print("Failed to add user: $error"));
+      } else {
+        reference
+            .doc('kZ5NRpx5WBxLOLwKPWwQ')
+            .update({'${foods.keys.elementAt(i)}' : foods[foods.keys.elementAt(i)][0]})
+            .then((value) => print("User Added"))
+            .catchError((error) => print("Failed to add user: $error"));
+      }
+    }
   }
 }
